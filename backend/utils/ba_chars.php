22c486300a82e19e08525f119167d3e892d58261
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-11-10 12:33:56 +0300

    create ba_chars function

---
+++ backend/utils/ba_chars.php
@@ -0,0 +1,21 @@
+<?php
+function ba_chars($word) {
+    // Отделяем до и после слова лишние знаки, если есть
+    $before_chars = '';
+    $after_chars = '';
+    $before_chars_pattern = '/^[^А-яӀ]+/u';
+    $after_chars_pattern = '/[^А-яӀ]+$/u';
+    // Получаем знаки перед словом 
+    if ( preg_match($before_chars_pattern, $word, $matches) ) {
+        $before_chars = $matches[0];
+        $word = preg_replace($before_chars_pattern, '', $word);
+    }
+    // Получаем знаки после слова        
+    if ( preg_match($after_chars_pattern, $word, $matches) ) {
+        $after_chars = $matches[0];
+        $word = preg_replace($after_chars_pattern, '', $word);
+    }
+
+    return [$before_chars, $after_chars];
+}
+?>
