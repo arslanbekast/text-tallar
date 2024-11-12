efcce6a2649edcef09057e9c6ba66e8bfc8d9ebd
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-11-12 11:54:22 +0300

    changes test.php

--- test.php
+++ test.php
@@ -1,10 +1,13 @@
 <?php
 include ("backend/utils/ba_chars.php");
+include ("backend/utils/convert_word.php");
 
-$word = "(=====абоде}}}}).";
-[$before_chars, $after_chars] = ba_chars($word);
-print_r($before_chars);
-echo "    ";
-print_r($after_chars);
+// $word = "(=====абоде}}}}).";
+// [$before_chars, $after_chars] = ba_chars($word);
+// print_r($before_chars);
+// echo "    ";
+// print_r($after_chars);
+
+print_r(convert_word('абоде'));
 
 ?>
