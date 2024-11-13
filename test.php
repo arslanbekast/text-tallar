6e24d914039882a253063fe3b829d1fece433a8b
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-11-13 11:05:34 +0300

    test

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
 include ("backend/utils/ba_chars.php");
 include ("backend/utils/convert_word.php");
+include ("backend/utils/clean_html.php");
 
 // $word = "(=====абоде}}}}).";
 // [$before_chars, $after_chars] = ba_chars($word);
@@ -8,6 +9,6 @@ include ("backend/utils/convert_word.php");
 // echo "    ";
 // print_r($after_chars);
 
-print_r(convert_word('абоде'));
+echo clean_html('<p>абоде</p> ')
 
 ?>
